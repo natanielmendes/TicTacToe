7a3a3e689c40a3ad8871118bb3cd7a32c9d8e92e
Nataniel Carvalho <natanielmendes@hotmail.com>
2018-08-03 14:09:25 -0300

    Main class removal, assert statements

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,28 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TicTacToeTest {
-
-    public static void main(String[] main) {
-        TicTacToe ticTacToeFirstWinner = new TicTacToe(3);
-        ticTacToeFirstWinner.move(0,0,1);
-        ticTacToeFirstWinner.move(1,0,2);
-        ticTacToeFirstWinner.move(0,1,1);
-        int noOneFirstWinner = ticTacToeFirstWinner.move(1,1,2);
-        int player1 = ticTacToeFirstWinner.move(0,2,1);
-
-        System.out.println("No one wins: " + noOneFirstWinner);
-        System.out.println("Player 1 wins: " + player1);
-
-        TicTacToe ticTacToeSecondWinner = new TicTacToe(3);
-        ticTacToeSecondWinner.move(0,0,2);
-        ticTacToeSecondWinner.move(1,0,1);
-        ticTacToeSecondWinner.move(0,1,2);
-        int noOneSecondWinner = ticTacToeSecondWinner.move(1,1,1);
-        int player2 = ticTacToeSecondWinner.move(0,2,2);
-
-        System.out.println("No one wins: " + noOneSecondWinner);
-        System.out.println("Player 2 wins: " + player2);
-    }
 
     @Test
     public void Player1Winner() {
@@ -32,6 +11,9 @@ public class TicTacToeTest {
         ticTacToeFirstWinner.move(0,1,1);
         int noOneFirstWinner = ticTacToeFirstWinner.move(1,1,2);
         int player1 = ticTacToeFirstWinner.move(0,2,1);
+
+        Assert.assertEquals(0, noOneFirstWinner); // No one statement
+        Assert.assertEquals(1, player1); // No one statement
 
         System.out.println("No one wins: " + noOneFirstWinner);
         System.out.println("Player 1 wins: " + player1);
@@ -46,6 +28,9 @@ public class TicTacToeTest {
         int noOneSecondWinner = ticTacToeSecondWinner.move(1,1,1);
         int player2 = ticTacToeSecondWinner.move(0,2,2);
 
+        Assert.assertEquals(0, noOneSecondWinner); // No one statement
+        Assert.assertEquals(2, player2); // No one statement
+
         System.out.println("No one wins: " + noOneSecondWinner);
         System.out.println("Player 2 wins: " + player2);
     }
@@ -58,6 +43,9 @@ public class TicTacToeTest {
         ticTacToeSecondWinner.move(1,1,2);
         int noOneSecondWinner = ticTacToeSecondWinner.move(0,1,1);
         int player2 = ticTacToeSecondWinner.move(2,2,2);
+
+        Assert.assertEquals(0, noOneSecondWinner); // No one statement
+        Assert.assertEquals(2, player2); // No one statement
 
         System.out.println("No one wins: " + noOneSecondWinner);
         System.out.println("Player 2 wins: " + player2);
